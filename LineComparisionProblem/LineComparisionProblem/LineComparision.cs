31373677b46eeb28add55fe056ccce019b19a2d7
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-02 16:23:20 +0530

    Compare to method Added

--- LineComparisionProblem/LineComparisionProblem/LineComparision.cs
+++ LineComparisionProblem/LineComparisionProblem/LineComparision.cs
@@ -21,6 +21,7 @@ namespace LineComparisionProblem
             int y2 = Convert.ToInt32(Console.ReadLine());
 
             double length_Of_1stLine = ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("The length of first line is : " + length_Of_1stLine);
             // Also use this predined Math.Sqrt method for solving equation
             // double length_Of_A_Line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 
@@ -35,18 +36,20 @@ namespace LineComparisionProblem
             int q2 = Convert.ToInt32(Console.ReadLine());
 
             double length_Of_2ndLine = Math.Sqrt(Math.Pow(p2 - p1,2) + Math.Pow(q2 - q1,2));
+            Console.WriteLine("the length of second line is : " + length_Of_2ndLine);
 
-            if (length_Of_1stLine == length_Of_2ndLine)
+            if (length_Of_2ndLine.CompareTo(length_Of_1stLine) < 0 )
             {
-                Console.WriteLine("\nBoth lines in equal length");
+                Console.WriteLine("\n 1st line is greater");
             }
-            if (length_Of_1stLine > length_Of_2ndLine)
+            else
+            if (length_Of_1stLine.CompareTo(length_Of_2ndLine) > 0 )
             {
-                Console.WriteLine("\n 1st line is greater than 2nd line ");
+                Console.WriteLine("\n 2nd line is greater");
             }
             else
             {
-                Console.WriteLine("\n  2nd line is greater than 1st line ");
+                Console.WriteLine("\n Both line are equal ");
 
             }
         }
